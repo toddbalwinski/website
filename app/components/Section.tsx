85c0e753537ca9648bdd45182d208ea1c530b824
Todd Balwinski <tbalwinski@gmail.com>
2025-08-09 14:08:30 -0700

    Create Section.tsx

---
+++ app/components/Section.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+export default function Section({
+  id,
+  className,
+  children,
+}: React.PropsWithChildren<{ id?: string; className?: string }>) {
+  return (
+    <section
+      id={id}
+      className={`relative px-6 sm:px-8 md:px-12 max-w-6xl mx-auto ${className || ""}`}
+    >
+      {children}
+    </section>
+  );
+}
